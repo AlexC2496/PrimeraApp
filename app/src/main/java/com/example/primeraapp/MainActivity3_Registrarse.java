42294184bd27d3456d736686b7945e21b1b0d695
Alex <acirjan01@educantabria.es>
2020-10-28 11:53:52 +0100

    25102020

--- app/src/main/java/com/example/primeraapp/MainActivity3_Registrarse.java
+++ app/src/main/java/com/example/primeraapp/MainActivity3_Registrarse.java
@@ -3,12 +3,22 @@ package com.example.primeraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity3_Registrarse extends AppCompatActivity {
+
+    TextView Opciones;
+    Spinner combo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity3__registrarse);
+        Opciones= (TextView) findViewById(R.id.etiSeleccion);
+        combo= (Spinner) findViewById(R.id.spinnerOpciones);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.combo, android.R.layout.simple_spinner_item);
+        combo.setAdapter(adapter);
     }
 }
