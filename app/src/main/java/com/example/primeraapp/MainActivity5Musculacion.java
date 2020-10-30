ac487ab30ed0f5cf9cdd9bab77f0e41a4c7eee69
Alex <acirjan01@educantabria.es>
2020-10-30 21:56:15 +0100

    30102020

---
+++ app/src/main/java/com/example/primeraapp/MainActivity5Musculacion.java
@@ -0,0 +1,14 @@
+package com.example.primeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity5Musculacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity5_musculacion);
+    }
+}
