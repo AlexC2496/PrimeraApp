4329ded44cddc101c3c8a171e79dcf990dbd949a
Alex <acirjan01@educantabria.es>
2021-02-11 13:54:57 +0100

    10022021

--- app/src/main/java/com/example/primeraapp/MainActivity.java
+++ app/src/main/java/com/example/primeraapp/MainActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MenuItem item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,23 @@ public class MainActivity extends AppCompatActivity {
     public void Mapa (View view) {
         Intent mapa = new Intent(this, MapsActivity.class);
         startActivity(mapa);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.overflow,menu);
+        return true;
+    }
+    //Metodo para asignar funciones
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id==R.id.item1){
+            Toast.makeText(this,"Opcion1",Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.item2){
+            Toast.makeText(this,"Opcion2",Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     }
