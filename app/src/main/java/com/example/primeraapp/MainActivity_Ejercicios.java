72436f98e167ba8850b946f53bf3f920cc9304d4
Alex <acirjan01@educantabria.es>
2021-02-11 13:09:49 +0100

    10022021

--- app/src/main/java/com/example/primeraapp/MainActivity_Ejercicios.java
+++ app/src/main/java/com/example/primeraapp/MainActivity_Ejercicios.java
@@ -11,15 +11,16 @@ public class MainActivity_Ejercicios extends AppCompatActivity {
 
     WebView webView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__ejercicios);
 
       webView= (WebView) findViewById(R.id.wv_test);
+
       WebSettings  webSettings = webView.getSettings();
       webSettings.setJavaScriptEnabled(true);
-
 
       webView.setWebViewClient(new WebViewClient());
       webView.loadUrl("https://www.youtube.com/watch?v=LBDqQA8AVD4&ab_channel=EntrenaconSergioPeinado");
