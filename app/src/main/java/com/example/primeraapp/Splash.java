4cbebeea5017154565dec42f11ea0153e99c4c40
Alex <acirjan01@educantabria.es>
2021-02-23 13:52:54 +0100

    10022021

---
+++ app/src/main/java/com/example/primeraapp/Splash.java
@@ -0,0 +1,29 @@
+package com.example.primeraapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    private final int Duracion_Splash = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, Duracion_Splash);
+    }
+}
