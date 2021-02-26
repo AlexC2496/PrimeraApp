c26072a61a91edcfc8c02dca88361e36e18cbca1
AlexC2496 <72732234+AlexC2496@users.noreply.github.com>
2021-02-26 17:34:37 +0100

    26022021

--- app/src/main/java/com/example/primeraapp/ejercicios_abdominales.java
+++ app/src/main/java/com/example/primeraapp/ejercicios_abdominales.java
@@ -2,10 +2,14 @@ package com.example.primeraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class ejercicios_abdominales extends AppCompatActivity {
     WebView webView;
@@ -21,5 +25,19 @@ public class ejercicios_abdominales extends AppCompatActivity {
 
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://www.youtube.com/watch?v=zjrH2fLnh1I&ab_channel=EntrenaconSergioPeinado");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_simple, menu);
+        return true;
+    }
+    public boolean onOptionsSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id == R.id.item2) {
+            Intent atras = new Intent(this, MainActivity.class);
+            startActivity(atras);
+            Toast.makeText(this, "Go back", Toast.LENGTH_SHORT).show();
+        }return super.onOptionsItemSelected(item);
     }
 }
