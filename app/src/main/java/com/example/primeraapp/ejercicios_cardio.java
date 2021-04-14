d17d012a62433bba5c8e182c05bdd62d56897921
AlexC2496 <72732234+AlexC2496@users.noreply.github.com>
2021-04-14 18:31:36 +0200

    15042021

--- app/src/main/java/com/example/primeraapp/ejercicios_cardio.java
+++ app/src/main/java/com/example/primeraapp/ejercicios_cardio.java
@@ -2,25 +2,24 @@ package com.example.primeraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.VideoView;
 
 public class ejercicios_cardio extends AppCompatActivity {
 
     WebView webView;
+    VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicios_cardio);
 
-        webView= (WebView) findViewById(R.id.wv_cardio);
-
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.youtube.com/watch?v=uSD5IxmEQdI&ab_channel=EntrenaconSergioPeinado");
+        videoView = (VideoView) findViewById(R.id.wv_cardio);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" +R.raw.video));
+        videoView.start();
     }
 }
