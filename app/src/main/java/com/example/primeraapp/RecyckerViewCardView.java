bc49089a51c7da20335e785d879043af09f45d86
Alex <acirjan01@educantabria.es>
2021-05-28 10:56:20 +0200

    03052021

---
+++ app/src/main/java/com/example/primeraapp/RecyckerViewCardView.java
@@ -0,0 +1,14 @@
+package com.example.primeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecyckerViewCardView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycker_view_card_view);
+    }
+}
